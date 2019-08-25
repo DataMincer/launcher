bbf0e7a1fbf522ba3317cb52db645257d0c81735
Artiom Neganov <aneganov@gmail.com>
2019-08-25 22:05:54 +0300

    Refactor: rename Deck into Unit

--- src/App.php
+++ src/App.php
@@ -9,7 +9,7 @@ use TaskRunner\LoggerInterface;
 class App extends \TaskRunner\App {
 
   /** @var Manager */
-  protected $deckManager;
+  protected $manager;
 
   protected static $paramsMap = [
     'basePath' => '--base',
@@ -19,9 +19,9 @@ class App extends \TaskRunner\App {
     'verbose' => '-v',
     'debug' => '--debug',
     'timer' => '--timer',
-    'deckId' => '--deck',
+    'unitId' => '--unit',
     'bundle' => 'bundle',
-    'deck' => 'deck',
+    'unit' => 'unit',
     'task' => 'TASK',
     'taskParams' => 'PARAMS',
     'help' => 'help'
@@ -33,7 +33,7 @@ class App extends \TaskRunner\App {
   public function __construct() {
     parent::__construct();
     try {
-      $this->deckManager = new Manager($this->options, $this->logger);
+      $this->manager = new Manager($this->options, $this->logger);
     }
     catch(Exception $e) {
       $this->logger()->err($e->getMessage());
@@ -42,15 +42,15 @@ class App extends \TaskRunner\App {
   }
 
   public function manager() {
-    return $this->deckManager;
+    return $this->manager;
   }
 
   protected function getUsageDefinition() {
     return <<<TXT
-Ultimate Audition: Deck Manager
+DataMincer Launcher
 
 Usage:
-  dm-dm (bundle | deck [--deck=DECK]) 
+  dm-dm (bundle | unit [--unit=DECK]) 
     (help | TASK [PARAMS...])
     [options] 
     [-v...]
@@ -61,19 +61,19 @@ Commands:
   bundle                Lists available bundles and their details.
   bundle TASK           Execute task TASK on available bundles.
   bundle help           List defined tasks.
-  deck                  Lists available decks and their details.
-  deck TASK             Execute task TASK on available decks.
-  deck help             List defined tasks.
+  unit                  Lists available units and their details.
+  unit TASK             Execute task TASK on available units.
+  unit help             List defined tasks.
 
 Options:
-  --base=PATH           Path to the dir with deck bundles data. [Default: ./]
-  --build=PATH          Path to the dir used to build decks. [Default: ./build]
-  --deck=DECK           Limit operations down to the single deck.
+  --base=PATH           Path to the dir with unit bundles data. [Default: ./]
+  --build=PATH          Path to the dir used to build units. [Default: ./build]
+  --unit=DECK           Limit operations down to the single unit.
   -v...                 Show more details when running some tasks. Repeating may increase verbosity.
   --novalidate          Don't validate schemas (speeds up execution, but may rise unhandled exceptions).
   --debug               Show debugging information when running some tasks.
   --timer               Enable timer.
-  --filter=FILTER...    Filter the decks to work with. Represents a list in the following format:
+  --filter=FILTER...    Filter the units to work with. Represents a list in the following format:
 
                           FILTER ::= [BUNDLE][:SELECTOR[;SELECTOR...]]
                           SELECTOR ::= SELECTOR(i)[(:SELECTOR(i+1))...]
@@ -96,22 +96,22 @@ Options:
                         the following invocations are possible:
 
                           basic-numbers:lang=en
-                            - Select decks which primary language is "en" (both British or American).
+                            - Select units which primary language is "en" (both British or American).
 
                           basic-numbers:lang=en.en-US
-                            - Select decks which primary language is "en" (American English).
+                            - Select units which primary language is "en" (American English).
 
                           basic-numbers:lang=en:fr
-                            - Select decks which primary language is "en" and native language is "fr".
+                            - Select units which primary language is "en" and native language is "fr".
 
                           basic-numbers:deck=numbers
-                            - Select decks from dimension "deck", domain "numbers".
+                            - Select units from dimension "deck", domain "numbers".
 
                           basic-numbers:lang=:ru\;deck=numbers
-                            - Select decks from dimension "deck" domain "numbers" and having "ru" as native language.
+                            - Select units from dimension "deck" domain "numbers" and having "ru" as native language.
 
                           basic-numbers:lang=en,fr
-                            - Select decks having primary language "en" or "fr"
+                            - Select units having primary language "en" or "fr"
 TXT;
   }
 
